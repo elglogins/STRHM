11107e4c20755841768b1faa77fae96bfebe31a8
Elgars Logins <elg.logins@gmail.com>
2018-03-30 15:25:52 +0200

    Base redis repository

---
+++ STRHM/STRHM/Repositories/BaseRedisHashSetRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using StackExchange.Redis;
+
+namespace STRHM.Repositories
+{
+    public abstract class BaseRedisHashSetRepository<T>
+    {
+        private static ConfigurationOptions _configurationOptions;
+
+        public BaseRedisHashSetRepository()
+        {
+            var conString = ConfigurationManager.ConnectionStrings["RedisConnectionString"].ConnectionString;
+            _configurationOptions = new ConfigurationOptions();
+            _configurationOptions.EndPoints.Add(conString);
+        }
+
+        #region Connection
+
+        protected static ConnectionMultiplexer GetConnection
+        {
+            get
+            {
+                return LazyConnection.Value;
+            }
+        }
+
+        protected static readonly Lazy<ConnectionMultiplexer> LazyConnection
+            = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                return ConnectionMultiplexer.Connect(_configurationOptions);
+            });
+
+        #endregion
+    }
+}
